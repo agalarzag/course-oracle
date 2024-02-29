db61a295722999d248144e75e4d925ec4ce0147a
Academico <Academico@T08KETIPC12A202.instructores.senati.local>
2024-02-29 13:22:01 -0500

    sql

---
+++ ejercicio03.sql
@@ -0,0 +1,16 @@
+--Realizar una base de datos
+ 
+--1. Listar todos los customer que inicien su id con la letra "A". 
+--Guardas el archivo como "ejercicio01.sql" y lo subes a tu repositorio
+
+select * from Customers where customerid like 'A%'
+
+--2. Listar a todos los customers que sean de los paises: france, germany,  sweden.
+-- Guardar el archi como  "ejercicio0.sql" u subes a tu respositorio
+
+select * from Customers where Country in ('france','germany','sweden')
+
+--3. Listar todos los orders que el campo shiregion no sean null
+-- Guardar el archi como  "ejercicio03.sql" u subes a tu respositorio
+
+select * from Orders where ShipRegion is not null
