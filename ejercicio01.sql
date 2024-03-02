45648121efbd66c6e460a15a3df720106e209925
agalarzag <161747348+agalarzag@users.noreply.github.com>
2024-03-02 12:26:33 -0500

    Update ejercicio01.sql

--- ejercicio01.sql
+++ ejercicio01.sql
@@ -4,3 +4,17 @@
 --Guardas el archivo como "ejercicio01.sql" y lo subes a tu repositorio
 
 select * from Customers where customerid like 'A%'
+
+git add .
+ git commit - m "sql"
+git push -u origin
+
+ cambiar de github
+git remote remove origin
+git remote add origin url...
+
+traer todos los datos
+ git pull origin main
+
+git branch 
+especifica las ramas
